9339ecf4b09eb53eeac1e3dd6f5f214ecca5c4f0
Max Patten <amanthus2535@gmail.com>
2019-05-22 09:51:24 -0700

    Removed .window elements with help from Travis at office hours, still works! :)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,35 +7,41 @@ var guessCounter = document.getElementById("guessCounter");
 var wins = 0
 var losses = 0
 var guessesLeft = 9
-
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+console.log("Computer chose letter: " + computerGuess);
 document.onkeyup = function (event) {
     var userGuess = event.key;
     if (guessesLeft === 9) {
-        window.computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
         guessesLeft--;
     }
-    if (guessesLeft > 0 && guessesLeft < 9 && userGuess === window.computerGuess) {
+    if (guessesLeft > 0 && guessesLeft < 9 && userGuess === computerGuess) {
         wins++;
         guessesLeft = 9;
-        // console.log("Computer chose letter: " + computerGuess);
+        // 
         // console.log("Player chose letter: " + userGuess);
         // console.log("You Won! Let's play again.");
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log("Computer chose letter: " + computerGuess);
+
         alert("You Won! Let's play again.")
-    } else if (guessesLeft === 8 && userGuess !== window.computerGuess) {
+    } else if (guessesLeft === 8 && userGuess !== computerGuess) {
         guessesLeft--;
         // console.log("Computer chose letter: " + computerGuess);
         // console.log("Player chose letter: " + userGuess);
         // console.log("Player has " + (guessesLeft + 1) + " guesses left.");
-    } else if (guessesLeft > 0 && guessesLeft < 8 && userGuess !== window.computerGuess) {
+    } else if (guessesLeft > 0 && guessesLeft < 8 && userGuess !== computerGuess) {
         guessesLeft--;
         // console.log("Computer chose letter: " + computerGuess);
         // console.log("Player chose letter: " + userGuess);
         // console.log("Player has " + (guessesLeft + 1) + " guesses left.");
-    } else if (guessesLeft === 0 && userGuess !== window.computerGuess) {
+    } else if (guessesLeft === 0 && userGuess !== computerGuess) {
         guessesLeft--;
         losses++;
         // console.log("You ran out of guesses. You lose!");
         guessesLeft = 9;
+        computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log("Computer chose letter: " + computerGuess);
+
         alert("You Lost! Let's play again.")
     }
     winsCounter.textContent = wins;
