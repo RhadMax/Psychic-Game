0b2359bb7690486d1db5b1fc683ebb692dfac3c1
Max Patten <amanthus2535@gmail.com>
2019-05-24 21:29:36 -0700

    Added visuals, added hidden youtube video for bg music, added taunt array for NPC

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,20 @@ var winsCounter = document.getElementById("winsCounter");
 var lossesCounter = document.getElementById("lossesCounter");
 var userGuessText = document.getElementById("userGuess");
 var guessCounter = document.getElementById("guessCounter");
+var alerter = document.getElementById("alerting");
 var wins = 0
 var losses = 0
 var guessesLeft = 9
+var taunts = ["Haha, not that one!", "Nope! Keep guessing!", "Ha! Some hero you are.", "Goblins guess better!", "Hee hee hee, try again!", "You'll never win like this!", "Your chances dwindle...", "I'll soon win!!", "I would never choose that!", "You will never win!", "Magic guards my mind"]
+// function toggleMute() {
+//     var music = $("#music");
+//     console.log(music);
+//     music.unMute();
+// }
+taunt = taunts[Math.floor(Math.random() * taunts.length)];
 
 document.onkeyup = function (event) {
+    taunt = taunts[Math.floor(Math.random() * taunts.length)];
     var userGuess = event.key;
     if (guessesLeft === 9) {
         window.computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -20,34 +29,38 @@ document.onkeyup = function (event) {
         // console.log("Computer chose letter: " + computerGuess);
         // console.log("Player chose letter: " + userGuess);
         // console.log("You Won! Let's play again.");
-        alert("You Won! Let's play again.")
+        // alert("You Won! Let's play again.");
+        alerter.textContent = "Dang! You guessed it!";
     } else if (guessesLeft === 8 && userGuess !== window.computerGuess) {
         guessesLeft--;
         // console.log("Computer chose letter: " + computerGuess);
         // console.log("Player chose letter: " + userGuess);
         // console.log("Player has " + (guessesLeft + 1) + " guesses left.");
+        alerter.textContent = taunt;
     } else if (guessesLeft > 0 && guessesLeft < 8 && userGuess !== window.computerGuess) {
         guessesLeft--;
         // console.log("Computer chose letter: " + computerGuess);
         // console.log("Player chose letter: " + userGuess);
         // console.log("Player has " + (guessesLeft + 1) + " guesses left.");
+        alerter.textContent = taunt;
     } else if (guessesLeft === 0 && userGuess !== window.computerGuess) {
         guessesLeft--;
         losses++;
         // console.log("You ran out of guesses. You lose!");
         guessesLeft = 9;
-        alert("You Lost! Let's play again.")
+        alerter.textContent = "Tee hee hee! I win!";
+        // alert("You Lost! Let's play again.")
     }
-    winsCounter.textContent = wins;
-    lossesCounter.textContent = losses;
-    if  (guessesLeft === 9) {
-        userGuessText.textContent = " ";
+    winsCounter.textContent = " " + wins + " ";
+    lossesCounter.textContent = " " + losses + " ";
+    if (guessesLeft === 9) {
+        userGuessText.textContent = "...";
     } else {
         userGuessText.textContent += (userGuess + ", ");
     }
     if (guessesLeft === 9) {
-        guessCounter.textContent = guessesLeft;
+        guessCounter.textContent = " " + guessesLeft + " ";
     } else {
-        guessCounter.textContent = (guessesLeft + 1);
+        guessCounter.textContent = " " + (guessesLeft + 1) + " ";
     }
 }
